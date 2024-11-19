b8a42d752dbea5efc529ee5326483b7b2008c3f4
Máté Dombóvári <63262118+Stalker9651@users.noreply.github.com>
2024-11-19 09:24:30 +0100

    Add project files.

---
+++ Orai1119/Dolgozo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orai1119
+{
+    internal class Dolgozo
+    {
+        public string Nev { get; set; }
+        public bool Nem { get; set; }
+        public string Reszleg { get; set; }
+        public int Belepes { get; set; }
+        public int Ber { get; set; }
+
+        public Dolgozo(string sor)
+        {
+            var split = sor.Split(";");
+            Nev = split[0];
+            Nem = split[1] == "férfi";
+            Reszleg = split[2];
+            Belepes = int.Parse(split[3]);
+            Ber = int.Parse(split[4]);
+        }
+        override public string ToString()
+        {
+            return $"{Nev} {(Nem ? "férfi" : "nő")} {Reszleg} {Belepes} {Ber}";
+        }
+
+    }
+}
---
+++ Orai1119/Program.cs
@@ -0,0 +1,42 @@
+﻿using Orai1119;
+
+List <Dolgozo> berek = new List<Dolgozo>();
+
+using (var sr = new StreamReader("../../../src/berek2020.txt"))
+{
+    _ = sr.ReadLine();
+    while (!sr.EndOfStream)
+	{
+        berek.Add(new Dolgozo(sr.ReadLine()));
+
+    }
+}
+
+// 3. feladat
+Console.WriteLine(berek.Count);
+
+// 4. feladat
+var f4 = berek.Average(x => x.Ber);
+Console.WriteLine($"{f4 / 1000:.0} ezer forint");
+
+// 5. feladat
+var input = Console.ReadLine();
+// 6. feladat
+if (berek.Any(x=> x.Reszleg == input))
+{
+var f5 = berek.Where(x=> x.Reszleg == input).MaxBy(x => x.Ber);
+Console.WriteLine($"{f5.ToString()}");
+}
+else
+{
+    Console.WriteLine("A megadott részleg nem létezik a cégnél!");
+}
+// 7. feladat
+var f7 = berek.GroupBy(x => x.Reszleg);
+foreach (var item in f7)
+{
+    Console.WriteLine($"{item.Key} {item.Count()}");
+}
+
+
+// 20 percig tartott
